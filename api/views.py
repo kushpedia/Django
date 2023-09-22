f8d95b138a98c4bd508a9ebd62129ff7eb81e717
kushpedia <haronkuria2015@gmail.com>
2023-09-22 21:18:29 +0300

    api

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 
 @api_view(['GET', 'POST'])
 def getRoutes(request):
@@ -29,4 +30,25 @@ def getAllProjects(request):
 def getProject(request,pk):
     project = Project.objects.filter(id=pk)
     serializer = ProjectSerializer(project, many=True)
+    return Response(serializer.data)
+
+
+# API FOR PROJECT VOTING
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request,pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+    review, created = Review.objects.get_or_create(
+        owner = user,
+        project = project,
+    )
+
+    review.value = data['value']
+    review.save()
+    project.getVoteCount
+
+    serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
